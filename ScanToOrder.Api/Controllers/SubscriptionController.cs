593ab39dbbbab5442981d108d2b456ee3dc888f5
Minhngo04 <minhnqse182368@fpt.edu.vn>
2026-02-24 15:33:07 +0700

    feat: subscription

---
+++ ScanToOrder.Api/Controllers/SubscriptionController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ScanToOrder.Application.Interfaces;
+using ScanToOrder.Application.Wrapper;
+
+namespace ScanToOrder.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubscriptionController : BaseController
+    {
+        private readonly ISubscriptionService _subscriptionService;
+        private readonly IAuthenticatedUserService _authenticatedUserService;
+
+        public SubscriptionController(ISubscriptionService subscriptionService, IAuthenticatedUserService authenticatedUserService)
+        {
+            _subscriptionService = subscriptionService;
+            _authenticatedUserService = authenticatedUserService;
+        }
+
+        [Authorize (Roles = "Tenant")]
+        [HttpPost]
+        public async Task<ActionResult<ApiResponse<string>>> Subscription([FromQuery] int planId)
+        {
+
+            var result = await _subscriptionService.SubscribePlanAsync(_authenticatedUserService.ProfileId.Value ,planId);
+            return Success(string.Empty, result);
+        }
+
+
+    }
+}
