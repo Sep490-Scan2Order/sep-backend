0aafcc7628f274ecb62b914dee0689bf21ebe78a
2109TruongNguyen <nntnnt2000@gmail.com>
2026-03-04 12:39:24 +0700

    feat: create promotion

---
+++ ScanToOrder.Application/Interfaces/IPromotionService.cs
@@ -0,0 +1,8 @@
+﻿using ScanToOrder.Application.DTOs.Promotion;
+
+namespace ScanToOrder.Application.Interfaces;
+
+public interface IPromotionService
+{
+    Task CreatePromotionAsync(Guid tenantId, CreatePromotionDto dto);
+}
---
+++ ScanToOrder.Infrastructure/Migrations/20260304030646_UpdatePromotionFields.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ScanToOrder.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatePromotionFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Priority",
+                table: "Promotions",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "PromotionDishes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    DishId = table.Column<int>(type: "integer", nullable: false),
+                    PromotionId = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PromotionDishes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PromotionDishes_Dishes_DishId",
+                        column: x => x.DishId,
+                        principalTable: "Dishes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PromotionDishes_Promotions_PromotionId",
+                        column: x => x.PromotionId,
+                        principalTable: "Promotions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PromotionDishes_DishId",
+                table: "PromotionDishes",
+                column: "DishId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PromotionDishes_PromotionId",
+                table: "PromotionDishes",
+                column: "PromotionId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PromotionDishes");
+
+            migrationBuilder.DropColumn(
+                name: "Priority",
+                table: "Promotions");
+        }
+    }
+}
