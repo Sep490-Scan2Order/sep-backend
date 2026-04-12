d67da940770179917e39bff2b2a6b12e050cdb8d
Minhngo04 <minhnqse182368@fpt.edu.vn>
2026-04-12 22:02:43 +0700

    refactor: add RefundedQuantity in kdsitemresponse

--- ScanToOrder.Application/DTOs/Orders/KdsItemResponse.cs
+++ ScanToOrder.Application/DTOs/Orders/KdsItemResponse.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +15,7 @@ namespace ScanToOrder.Application.DTOs.Orders
         public decimal PromotionAmount { get; set; }
 
         public int Quantity { get; set; }
+        public int RefundedQuantity { get; set; }
         public string Image { get; set; }
     }
 }
